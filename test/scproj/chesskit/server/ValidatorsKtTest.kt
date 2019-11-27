5c420f0c2962387451cebd78acee93f2c8c61345
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2019-11-28 02:26:15 +0800

    Rewrite server
    Decomposing the monster into test-friendly small pure
    higher order functions, dirty state is injected only at the final stage.

---
+++ test/scproj/chesskit/server/ValidatorsKtTest.kt
@@ -0,0 +1,30 @@
+package scproj.chesskit.server
+
+import org.junit.jupiter.api.Test
+
+internal class ValidatorsKtTest {
+
+    @Test
+    fun checkPlayerSide() {
+    }
+
+    @Test
+    fun checkMovement() {
+    }
+
+    @Test
+    fun checkRevert() {
+    }
+
+    @Test
+    fun checkEndgame() {
+    }
+
+    @Test
+    fun checkServerStatus() {
+    }
+
+    @Test
+    fun checkOccupation() {
+    }
+}
