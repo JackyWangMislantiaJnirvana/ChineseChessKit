9a231034bd3cb63e0c2b7febbac9417bdd8d6540
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2019-12-10 11:25:35 +0800

    finish drawing stuff

---
+++ src/DemoChessPiece.kt
@@ -0,0 +1,27 @@
+import javafx.geometry.Point2D
+import scproj.chesskit.client.ChessPieceImageViewProxy
+import scproj.chesskit.client.pixelToGridCoordinate
+import tornadofx.View
+import tornadofx.imageview
+import tornadofx.pane
+
+class DemoChessPiece : View("My View") {
+    var chessPiece: ChessPieceImageViewProxy? = null
+    override val root = pane {
+        setMinSize(770.0, 840.0)
+        imageview("chessboard_smaller.jpg") {
+            setOnMouseClicked {
+                //                chessPiece!!.move(chessPiece!!.pixelToGridCoordinate(Point2D(it.sceneX, it.sceneY)))
+                println("x = ${it.sceneX}, y = ${it.sceneY}")
+                val coordinate = pixelToGridCoordinate(Point2D(it.sceneX, it.sceneY))
+                println("row = ${coordinate.positionX}, col = ${coordinate.positionY}")
+                chessPiece!!.move(coordinate)
+                println("chess at ${chessPiece!!.gridCoordinate}")
+            }
+        }
+        chessPiece = ChessPieceImageViewProxy(imageview("red_general.png") {
+            layoutX = -73.0
+            layoutY = -73.0
+        })
+    }
+}
---
+++ src/scproj/chesskit/client/ChessPieceImageViewProxy.kt
@@ -0,0 +1,88 @@
+package scproj.chesskit.client
+
+import javafx.geometry.Point2D
+import javafx.scene.image.ImageView
+import javafx.util.Duration
+import scproj.chesskit.core.data.Coordinate
+import tornadofx.move
+
+val PIECE_HEIGHT: Double = 73.0
+val PIECE_WIDTH: Double = 73.0
+
+val rowPixelAnchors = listOf(
+    27.0, 112.0, 200.0, 288.0, 387.0, 465.0, 555.0, 642.0, 731.0, 817.0
+)
+val columnPixelAnchors = listOf(
+    22.0, 109.0, 200.0, 291.0, 383.0, 474.0, 565.0, 656.0, 747.0
+)
+
+fun gridToPixelCoordinate(coordinate: Coordinate): Point2D =
+    Point2D(columnPixelAnchors[coordinate.positionY], rowPixelAnchors[coordinate.positionX])
+
+fun pixelToGridCoordinate(point: Point2D): Coordinate {
+    var coordinateX: Int = -1
+    var coordinateY: Int = -1
+    if (point.y <= rowPixelAnchors.first()) {
+        coordinateX = 0
+    } else if (point.y >= rowPixelAnchors.last()) {
+        coordinateX = rowPixelAnchors.size - 1
+    } else for (i in 0 until rowPixelAnchors.size - 1) {
+        if (point.y >= rowPixelAnchors[i] && point.y <= rowPixelAnchors[i + 1]) {
+            val intervalMiddlePoint = (rowPixelAnchors[i] + rowPixelAnchors[i + 1]) / 2.0
+            if (point.y <= intervalMiddlePoint) {
+                coordinateX = i
+                break
+            } else {
+                coordinateX = i + 1
+                break
+            }
+        }
+    }
+
+    if (point.x <= columnPixelAnchors.first()) {
+        coordinateY = 0
+    } else if (point.x >= columnPixelAnchors.last()) {
+        coordinateY = columnPixelAnchors.size - 1
+    } else for (i in 0 until columnPixelAnchors.size - 1) {
+        if (point.x >= columnPixelAnchors[i] && point.x <= columnPixelAnchors[i + 1]) {
+            val intervalMiddlePoint = (columnPixelAnchors[i] + columnPixelAnchors[i + 1]) / 2.0
+            if (point.x <= intervalMiddlePoint) {
+                coordinateY = i
+                break
+            } else {
+                coordinateY = i + 1
+                break
+            }
+        }
+    }
+    return Coordinate(coordinateX, coordinateY)
+}
+
+class ChessPieceImageViewProxy(val imageView: ImageView) {
+    // Position (-1, -1) is where these image rest when game is not started
+    val REST_POSITION = Coordinate(-1, -1)
+    val DURATION = Duration(1000.0)
+    var gridCoordinate = REST_POSITION
+        get() = field
+        private set(value) {
+            field = value
+        }
+
+    fun move(dest: Coordinate): ChessPieceImageViewProxy {
+        val clickedPixel = gridToPixelCoordinate(dest)
+        val restOffsetX = PIECE_WIDTH
+        val restOffsetY = PIECE_HEIGHT
+        val bulkOffsetX = -PIECE_WIDTH / 2.0
+        val bulkOffsetY = -PIECE_HEIGHT / 2.0
+        val translateVector: Point2D =
+            Point2D(clickedPixel.x + restOffsetX + bulkOffsetX, clickedPixel.y + restOffsetY + bulkOffsetY)
+
+        gridCoordinate = dest
+        imageView.move(DURATION, translateVector)
+        return this
+    }
+}
+
+fun ImageView.getProxy(): ChessPieceImageViewProxy {
+    return ChessPieceImageViewProxy(this)
+}
