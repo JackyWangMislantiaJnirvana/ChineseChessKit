94e726e8c63a80e3111e6193f512c27a19501a54
郭子笑 <c4computer>
2019-12-14 23:04:41 +0800

    Very stupid AI

--- src/scproj/chesskit/core/chess/BestMovement.java
+++ src/scproj/chesskit/core/chess/BestMovement.java
@@ -6,7 +6,7 @@ import scproj.chesskit.core.data.PlayerSide;
 import java.util.List;
 
 public class BestMovement {
-    /*public static Movement BestMovement(ChessGrid chessGrid, PlayerSide playerSide){
+    public static Movement BestMovement(ChessGrid chessGrid, PlayerSide playerSide){
         ChessGridElement[][] grid=chessGrid.getGrid();
         List<CanMove> movesRed=CanMoveList.GetRedCanMove(chessGrid);
         List<CanMove> movesBlack=CanMoveList.GetBlackCanMove(chessGrid);
@@ -34,8 +34,34 @@ public class BestMovement {
                 else if(piece==ChessGridElement.BLACK_SOLDIER){
                     return new Movement(PlayerSide.RED,click.starti,click.startj,click.endi,click.endj,false);
                 }
-                else
             }
         }
-    }*/
+        else if(playerSide==PlayerSide.BLACK){
+            for(CanMove click:movesBlack){
+                ChessGridElement piece=grid[click.endi][click.endj];
+                if(piece==ChessGridElement.RED_GENERAL){
+                    return new Movement(PlayerSide.BLACK,click.starti,click.startj,click.endi,click.endj,false);
+                }
+                else if(piece==ChessGridElement.RED_VEHICLE){
+                    return new Movement(PlayerSide.BLACK,click.starti,click.startj,click.endi,click.endj,false);
+                }
+                else if(piece==ChessGridElement.RED_RIDER){
+                    return new Movement(PlayerSide.BLACK,click.starti,click.startj,click.endi,click.endj,false);
+                }
+                else if(piece==ChessGridElement.RED_CANNON){
+                    return new Movement(PlayerSide.BLACK,click.starti,click.startj,click.endi,click.endj,false);
+                }
+                else if(piece==ChessGridElement.RED_MINISTER){
+                    return new Movement(PlayerSide.BLACK,click.starti,click.startj,click.endi,click.endj,false);
+                }
+                else if(piece==ChessGridElement.RED_SERVANT){
+                    return new Movement(PlayerSide.BLACK,click.starti,click.startj,click.endi,click.endj,false);
+                }
+                else if(piece==ChessGridElement.RED_SOLDIER){
+                    return new Movement(PlayerSide.BLACK,click.starti,click.startj,click.endi,click.endj,false);
+                }
+            }
+        }
+        return null;
+    }
 }
