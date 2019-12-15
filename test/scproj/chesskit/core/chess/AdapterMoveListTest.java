a1f1902a6dc2027755d59dd1bf6485a7f735a347
郭子笑 <c4computer>
2019-12-15 13:57:45 +0800

    okokokokok3

--- test/scproj/chesskit/core/chess/AdapterMoveListTest.java
+++ test/scproj/chesskit/core/chess/AdapterMoveListTest.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 class AdapterMoveListTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String[] s = new String[87];
-        for (int i = 0; i < 87; i++) {
+        String[] s = new String[14];
+        for (int i = 0; i < 14; i++) {
             s[i] = scan.nextLine();
         }
-//        Adapter1Result result1 = Adapter.adapter(new String[]);
-        Adapter2Result result2 = AdapterMoveList.AdapterMoveList(s, RulesKt.getDEFAULT_CHESSPLATE(), PlayerSide.RED);
-        GameStatus gameStatus=new GameStatus(result2.moveList,0);
-        ChessGridElement[][] grid=RebuildChessGrid.rebuildChessGrid(gameStatus,RulesKt.getDEFAULT_CHESSPLATE());
+        Adapter1Result result1 = Adapter.adapter(s);
+        //Adapter2Result result2 = AdapterMoveList.AdapterMoveList(s, RulesKt.getDEFAULT_CHESSPLATE(), PlayerSide.RED);
+        //GameStatus gameStatus=new GameStatus(result2.moveList,0);
+        //ChessGridElement[][] grid=RebuildChessGrid.rebuildChessGrid(gameStatus,RulesKt.getDEFAULT_CHESSPLATE());
         System.out.println();
     }
 }
